d9b5fa4035061c31205f48450bd3a9794cf6ed0f
jyb00 <jyb00003@gmail.com>
2024-08-16 14:15:24 +0900

    log

--- src/DxfViewer.js
+++ src/DxfViewer.js
@@ -1012,6 +1012,7 @@ class Batch {
      */
     _GetLineMaterial(key, color) {
         console.log(this.lineTypes, "lineType list");
+        console.log(color, "color");
         const mat = this.viewer._CreateLineMaterialInstance(this.viewer._TransformColor(color), this.lineTypes);
         if (key.lineType == 0 || key.lineType == "") {
             return new three.LineBasicMaterial({color: color});
@@ -1020,7 +1021,7 @@ class Batch {
         if (result.patternLength < 2) {
             return new three.LineBasicMaterial({color: color});
         }
-        const lineMaterial = this.viewer._CreateLineMaterialInstance(this.viewer._TransformColor('#a172c6'), this.lineTypes);
+        const lineMaterial = this.viewer._CreateLineMaterialInstance(this.viewer._TransformColor(color), this.lineTypes);
         
         return lineMaterial;
     }
