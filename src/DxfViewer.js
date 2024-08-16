f1a1d2637ce319435af0af9d7748deeb66b0901f
jyb00 <jyb00003@gmail.com>
2024-08-16 17:31:48 +0900

    check pattern array

--- src/DxfViewer.js
+++ src/DxfViewer.js
@@ -1028,11 +1028,12 @@ class Batch {
     _GetLineMaterial(key, color) {
         console.log(this.lineTypes, "lineType list");
         console.log(key, "key");
+        const result = this.lineTypes.find(el => el.name == key.lineType);
+        
         if (key.lineType == 0 || key.lineType == "") {
             return new three.LineBasicMaterial({color: color});
         }
-        const result = this.lineTypes.find(el => el.name == key.lineType);
-        if (result.pattern.length < 2) {
+        if (!result.pattern || result.pattern.length < 2) {
             return new three.LineBasicMaterial({color: color});
         }
         console.log(result, 'line info');
