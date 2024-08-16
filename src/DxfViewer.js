e363039709a83e81e5d15ae907a8e4c923762e5b
jyb00 <jyb00003@gmail.com>
2024-08-16 17:20:19 +0900

    add log

--- src/DxfViewer.js
+++ src/DxfViewer.js
@@ -1027,6 +1027,7 @@ class Batch {
      */
     _GetLineMaterial(key, color) {
         console.log(this.lineTypes, "lineType list");
+        console.log(key, "key");
         const mat = this.viewer._CreateLineMaterialInstance(this.viewer._TransformColor(color), this.lineTypes);
         if (key.lineType == 0 || key.lineType == "") {
             return new three.LineBasicMaterial({color: color});
